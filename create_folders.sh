dacf834c0cb0207baefec5d5a22e487ed9969af4
Barbara Diaz <debi_1223@hotmail.com>
2022-01-06 17:44:37 -0600

    create_folders file

---
+++ create_folders.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PHASES=(
+    phase1
+)
+for PHASE in "${PHASES[@]}"
+do
+mkdir -p \
+    data\
+    ${PHASE}/driver_code/{metrics,ensemble,validations}\
+    ${PHASE}/support_functions/{dt,lrg,pca,rf,svm}\
+    ${PHASE}/results/metrics_results/{metrics_plots,metrics_reports,filtered_results,metrics_stats}\
+    ${PHASE}/results/validation_results\
+    ${PHASE}/results/trained_results/{id_models,trained_models}\
+    ${PHASE}/results/trained_results/models_reports/coeff_reports\
+    ${PHASE}/results/trained_results/roc_plot/roc_data\
+
+done
