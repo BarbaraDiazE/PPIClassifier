b78cfcbb21f4d88a1868037306c8d80184e5f829
Barbara Diaz <debi_1223@hotmail.com>
2022-01-10 21:33:17 -0600

    information models

--- create_folders.sh
+++ create_folders.sh
@@ -11,7 +11,7 @@ mkdir -p \
     ${PHASE}/support_functions/{dt,lrg,pca,rf,svm}\
     ${PHASE}/results/metrics_results/{metrics_plots,metrics_reports,filtered_results,metrics_stats}\
     ${PHASE}/results/validation_results\
-    ${PHASE}/results/trained_results/{id_models,trained_models}\
+    ${PHASE}/results/trained_results/{id_models,information_models,trained_models}\
     ${PHASE}/results/trained_results/models_reports/coeff_reports\
     ${PHASE}/results/trained_results/roc_plot/roc_data\
     ${PHASE}/results/trained_results/tree_plot
