51cbdd5c840daa1e522654f3b6d0e63f5bae017e
Barbara Diaz <debi_1223@hotmail.com>
2022-02-04 12:21:14 -0600

    create get_metrics_by_algorithms.py to get summary metrics file and update headmap_plot

--- phase1/driver_code/metrics/headmap_plot.py
+++ phase1/driver_code/metrics/headmap_plot.py
@@ -36,7 +36,7 @@ class PlotHeadmap:
         files_list = [pk + ".csv" for pk in pk_models]
         return numerical_id, pk_models, files_list
 
-    def get_report_value(self, filename: str) -> float:
+    def get_report_value(self, filename: str) -> list:
         data_root = f"{self.local_root}/results/trained_results/models_reports"
         df = pd.read_csv(os.path.join(data_root, filename), sep=",", index_col="Unnamed: 0")
         model_metrics_values = [float(df.loc[metric].value) for metric in self.metrics]
@@ -48,6 +48,7 @@ class PlotHeadmap:
         values_list = [self.get_report_value(i) for i in files_list]
         data = np.array(values_list)
         df = pd.DataFrame(data=data, columns=self.metrics, index=numerical_id)
+        print(df)
         return df, numerical_id
 
     def plot(self):
@@ -97,10 +98,10 @@ if __name__ == "__main__":
     algorithms = ["svm", "lrg", "rf", "dt"]
     local_root = local_root["phase1"]
     # Q2
-    for algorithm in algorithms:
-        p = PlotHeadmap(algorithm, local_root, "Q2")
+    for algorithm_ in algorithms:
+        p = PlotHeadmap(algorithm_, local_root, "Q2")
         p.plot()
     # Q3
-    for algorithm in algorithms:
-        p = PlotHeadmap(algorithm, local_root, "Q3")
-        p.plot()
+    # for algorithm in algorithms:
+    #     p = PlotHeadmap(algorithm, local_root, "Q3")
+    #     p.plot()
